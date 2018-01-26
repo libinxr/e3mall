eeb3fdd42d21eb6c2961d888f4b2b3bfa9a3f708
libinxr <1161028763@qq.com>
2018-01-26 11:33:34 +0800

    第二天

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemExample;
@@ -29,6 +33,22 @@ public class ItemServiceImpl implements ItemService {
 		if(selectByExample!=null)
 			return selectByExample.get(0);
 		return null;
+	}
+
+
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example=new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		DataGridResult result=new DataGridResult();
+		result.setRows(list);
+		//取分页结果
+		PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+		result.setTotal(pageInfo.getTotal());
+		
+		return result;
 	}
 
 }
